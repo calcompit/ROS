bdcfe2312fa4af04099e2aebc9cf1b7448fe9668
Planet-tulip <planet.tulip.music@gmail.com>
2025-08-15 12:34:48 +0700

    Increase search bar spacing - Change margin-left from ml-6 to ml-8 for better visual separation from logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -56,7 +56,7 @@ const Header: React.FC<HeaderProps> = ({ onNotificationClick, ticketCount }) => 
           </div>
           
           {/* Search Bar */}
-          <div className="hidden md:flex max-w-md ml-6">
+          <div className="hidden md:flex max-w-md ml-8">
             <div className="relative">
               <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
               <Input
